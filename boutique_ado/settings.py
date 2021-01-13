bb7919d9d9d991cd15abdf6d8045bc50d2f1abb2
nafeesahyounis <nafeesah.youniss@gmail.com>
2021-01-13 15:01:04 +0000

    added /success to test allauth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -93,7 +93,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/success'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
